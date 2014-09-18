557782750d198cd04759875c5f09cceb9b98468f
fahchen <dev.fah@gmail.com>
2014-09-18 12:53:59 +0800

    Expose error_code of ResponseError

--- lib/wechat-rails.rb
+++ lib/wechat-rails.rb
@@ -9,7 +9,7 @@ module Wechat
   class ResponseError < StandardError
     attr_reader :error_code
     def initialize(errcode, errmsg)
-      error_code = errcode
+      @error_code = errcode
       super "#{errmsg}(#{error_code})"
     end
   end
